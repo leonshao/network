5bc0a08fff1bbeea4a932373eabcde427d9fd48c
leonshao <shao830417@163.com>
2012-12-12 17:26:22 +0800

    Revert "Revert "implement read/write interaction between client and server for non-block reading, ignore EAGAIN until successfully read data to buf""
    This reverts commit 8ebc67344727e81edce285e903ee35de7f33f265.

--- network/src/mytiny.c
+++ network/src/mytiny.c
@@ -6,10 +6,33 @@
  */
 
 #include "socketutil.h"
-#include <unistd.h>		// socklen_t, close()
+#include <errno.h>			// errno, EAGAIN
+
+void process_req(int fd) {
+	int readbytes, flags;
+	char buf[BUF_LEN];
+
+	// set fd to nonblock, otherwise it blocks on read()
+	flags = fcntl(fd, F_GETFL, 0);
+	if (flags < 0) {
+		flags = 0;
+	}
+	fcntl(fd, F_SETFL, flags|O_NONBLOCK);
+
+	// encouter EAGAIN, ignore it and read until resourse available
+	while((readbytes = read(fd, (char *)&buf, sizeof(buf))) == -1
+			&& errno == EAGAIN);
+	printf("server receive data: %s\t readbytes: %d\n", (char *)&buf, readbytes);
+
+	bzero((char *)&buf, sizeof(buf));
+
+	snprintf((char *)&buf, sizeof(buf), "Hello world.");
+	write(fd, (char *)&buf, strlen((char *)&buf) + 1);
+
+}
 
 int main(int argc, char** argv) {
-	int listenfd, port, clientfd, clientaddrlen;
+	int listenfd, port, connfd, clientaddrlen;
 	struct sockaddr_in clientaddr;
 
 	if (argc != 2) {
@@ -29,12 +52,15 @@ int main(int argc, char** argv) {
 
 	clientaddrlen = sizeof(clientaddr);
 	while(1){
-		clientfd = accept(listenfd, (SA *)&clientaddr, (socklen_t *)&clientaddrlen);
-		if (clientfd > 0) {
+		connfd = accept(listenfd, (SA *)&clientaddr, (socklen_t *)&clientaddrlen);
+		if (connfd > 0) {
 			// process request
 			printf("Get connection from %s:%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+			process_req(connfd);
 		}
-		close(listenfd);
+
+		// do not mistake to listenfd!!!
+		close(connfd);
 	}
 
 }
