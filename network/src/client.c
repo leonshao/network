bd43957247accb9fe37cf70a69e6f7bf21312ad3
leonshao <shao830417@163.com>
2012-12-12 17:19:40 +0800

    implement read/write interaction between client and server for non-block reading, ignore EAGAIN until successfully read data to buf

--- network/src/client.c
+++ network/src/client.c
@@ -7,6 +7,21 @@
 
 #include "socketutil.h"
 
+void send_req(int fd) {
+	char buf[BUF_LEN];
+	snprintf((char *)&buf, sizeof(buf), "Hi.");
+	write(fd, (char *)&buf, strlen((char *)&buf) + 1);
+}
+
+void process_resp(int fd) {
+	int recvbytes;
+	char buf[BUF_LEN];
+
+	read(fd, (char *)&buf, sizeof(buf));
+	printf("client receive data: %s\n", (char *)&buf);
+
+}
+
 int main(int argc, char** argv) {
 	int clientfd, port;
 	char *hostname;
@@ -21,6 +36,11 @@ int main(int argc, char** argv) {
 	if ((clientfd = open_clientfd(hostname, port)) < 0) {
 		return EXIT_FAILURE;
 	}
+
+	send_req(clientfd);
+	process_resp(clientfd);
+
+	close(clientfd);
 
 	return EXIT_SUCCESS;
 }
